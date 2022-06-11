d99e6f79056b943e0672bd729ecd875422b3644e
Александр Водяницкий <alexvod550@yandex.ru>
2022-06-11 10:55:34 +0300

    comments, pictures and follows

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('posts/<int:post_id>/edit/', views.post_edit, name='edit'),
     path('posts/<int:post_id>/comment/',
          views.add_comment,
-         name='add_comment'
-        ),
+         name='add_comment'),
     path('follow/', views.follow_index, name='follow_index'),
     path(
         'profile/<str:username>/follow/',
